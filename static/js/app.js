da8b318d8a9d73457ad54e283da1fbddf284d613
Rebecca Levine <belen.levine17@gmail.com>
2021-01-11 21:37:54 -0700

    final adjsutments

--- static/js/app.js
+++ static/js/app.js
@@ -1,31 +1,10 @@
 // Grab user input field
 var dropDown = d3.select("#selDataset");
 
-// Get metadata div
+// Get demographic, bar chart, and bubble chart divs
 var demoTable = d3.select("#sample-metadata");
-
-// Get bar chart and bubble chart divs
 var barChart = d3.select("#bar");
 var bubbleChart = d3.select("#bubble");
-
-// Create init screen
-function init() {
-    // Read json file
-    d3.json("data/samples.json").then(function(data) {
-        
-        // Get items for the drop down
-        var dropDownItems = data.names;
-
-        // Add the dropDownItems to the dropDown menu
-        for (var x = 0; x < dropDownItems.length; x++) {
-            dropDown.append("option").text(dropDownItems[x]);
-        };
-
-        // Display data and plots
-        getStats(dropDownItems[0]);
-        createCharts(dropDownItems[0]);
-    });
-};
 
 // Function to get metadata
 function getStats(id) {
@@ -186,6 +165,25 @@ function createCharts(id) {
 function optionChanged(id) {
     getStats(id);
     createCharts(id);
+};
+
+// Create init screen
+function init() {
+    // Read json file
+    d3.json("data/samples.json").then(function(data) {
+        
+        // Get items for the drop down
+        var dropDownItems = data.names;
+
+        // Add the dropDownItems to the dropDown menu
+        for (var x = 0; x < dropDownItems.length; x++) {
+            dropDown.append("option").text(dropDownItems[x]);
+        };
+
+        // Display data and plots
+        getStats(dropDownItems[0]);
+        createCharts(dropDownItems[0]);
+    });
 };
 
 init();
